604fbd7ca956d3b3a33a7943b92035d8933d3fcb
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-09-24 13:34:01 -0600

    Creacion de los Endpoints De Ls Entidades

---
+++ API_RESTful/Controllers/AdministradorController.cs
@@ -0,0 +1,91 @@
+﻿using API_RESTful.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Transferencia_Datos.Administrador_DTO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_RESTful.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdministradorController : ControllerBase
+    {
+        // Representa La DB:
+        private readonly MyDBcontext _MyDBcontext;
+
+        // Constructor:
+        public AdministradorController(MyDBcontext myDBcontext)
+        {
+            _MyDBcontext = myDBcontext;
+        }
+
+
+
+        // OBTIENE TODOS LOS REGISTROS DE LA DB:
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Administrador> Objetos_Obtenidos = await _MyDBcontext.Administradores.ToListAsync();
+
+            List<ObtenerPorId_DTO> Lista_Administradores = new List<ObtenerPorId_DTO>();
+
+            // Agregamos los registros obtenidos a la lista que mandaremos:
+            foreach (Administrador administrador in Objetos_Obtenidos)
+            {
+                Lista_Administradores.Add(new ObtenerPorId_DTO
+                {
+                    IdAdmin = administrador.IdAdmin,
+                    Nombre = administrador.Nombre,
+                    Email = administrador.Email,
+                    Contraseña=administrador.Contraseña,
+                    Rol=administrador.Rol
+                });
+            }
+
+            return Ok(Lista_Administradores);
+        }
+
+
+        // RECIBE UN OBJETO Y LO GUARDA EN LA DB:
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Crear_Administrador_DTO crear_Administrador_DTO)
+        {
+            // Objeto a Guardar:
+            Administrador administrador = new Administrador
+            {
+                Nombre = crear_Administrador_DTO.Nombre,
+                Email = crear_Administrador_DTO.Email,
+                Contraseña = crear_Administrador_DTO.Contraseña,
+                Rol = crear_Administrador_DTO.Rol
+            };
+
+            _MyDBcontext.Add(administrador);
+            await _MyDBcontext.SaveChangesAsync();
+
+            return Ok("Guardado Correctamente");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Administrador Objeto_Obtenido = await _MyDBcontext.Administradores.FirstOrDefaultAsync(x => x.IdAdmin == id);
+
+            if (Objeto_Obtenido != null)
+            {
+                _MyDBcontext.Remove(Objeto_Obtenido);
+                await _MyDBcontext.SaveChangesAsync();
+
+                return Ok("Administrador Eliminado Corectamente.");
+            }
+            else
+            {
+                return NotFound("No Se Encontro El Registro.");
+            }
+
+        }
+
+
+    }
+}
---
+++ API_RESTful/Controllers/ProductoController.cs
@@ -0,0 +1,86 @@
+﻿using API_RESTful.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Transferencia_Datos.Producto_DTO;
+
+
+namespace API_RESTful.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        // Representa La DB:
+        private readonly MyDBcontext _MyDBcontext;
+
+        // Constructor:
+        public ProductoController(MyDBcontext myDBcontext)
+        {
+            _MyDBcontext = myDBcontext;
+        }
+
+
+
+        // OBTIENE TODOS LOS REGISTROS DE LA DB:
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Producto> Objetos_Obtenidos = await _MyDBcontext.Productos.ToListAsync();
+
+            List<ObtenerPorId_DTO> Lista_Productos = new List<ObtenerPorId_DTO>();
+
+            // Agregamos los registros obtenidos a la lista que mandaremos:
+            foreach(Producto producto in Objetos_Obtenidos)
+            {
+                Lista_Productos.Add(new ObtenerPorId_DTO 
+                {
+                    IdProducto= producto.IdProducto,
+                    Nombre=producto.Nombre,
+                    Precio=producto.Precio
+                });
+            }
+
+            return Ok(Lista_Productos);
+        }
+
+
+        // RECIBE UN OBJETO Y LO GUARDA EN LA DB:
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Crear_Producto_DTO crear_Producto_DTO)
+        {
+            // Objeto a Guardar:
+            Producto producto = new Producto 
+            {
+                Nombre= crear_Producto_DTO.Nombre,
+                Precio=crear_Producto_DTO.Precio
+            };
+
+            _MyDBcontext.Add(producto);
+            await _MyDBcontext.SaveChangesAsync();
+
+            return Ok("Guardado Correctamente");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Producto Objeto_Obtenido = await _MyDBcontext.Productos.FirstOrDefaultAsync(x => x.IdProducto == id);
+
+            if(Objeto_Obtenido!=null)
+            {
+                _MyDBcontext.Remove(Objeto_Obtenido);
+                await _MyDBcontext.SaveChangesAsync();
+
+                return Ok("Producto Eliminado Corectamente.");
+            }
+            else
+            {
+                return NotFound("No Se Encontro El Registro.");
+            }
+
+        }
+
+
+    }
+}
